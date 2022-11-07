4c185e756eea64d08b0c19264a116cb658bf3a96
RPhilipson00 <78797518+RPhilipson00@users.noreply.github.com>
2022-11-07 16:14:25 +0000

    Add files via upload

--- reddit scraper.py
+++ reddit scraper.py
@@ -8,31 +8,36 @@ redditRead = praw.Reddit(client_id="ZHd4uF6mhAs2e8_rFnJLnA",
                                 username="Dissbot2000",
                                 password="MEGAsecretdisspass2022")
 #Creates an authorised praw instance to read reddit
-posts_dict1 = {"Post ID": [],"Title": [], "Post Text": []}
+posts_dict1 = {"Post_ID": [],"Title": [], "Post_Text": [], "User_Hash": [], "orientation": []}
 #initialises pandas dataframe
 
 
-def scraper(sub):
+def scraper(sub, orientation):
         posts = redditRead.subreddit(sub)
-        #redditor = reddit.redditor(redditor_name)
         #reads the subreddit
-        print("reading sub:", sub) #debugging line
-        for post in posts.hot(limit=1000):  #update to bigger number 
+        print("reading sub:", sub) #debugging line, lets me know the loop is running
+        for post in posts.hot(limit=100):  #goes through a maximum of the top 1000 posts on a subreddit, retrieving information
                 posts_dict1["Title"].append(post.title)
-                posts_dict1["Post Text"].append(post.selftext)
-                posts_dict1["Post ID"].append(post.id)
-                
-                #posts_dict1["User"].append(post.author)
+                posts_dict1["Post_Text"].append(post.selftext)
+                posts_dict1["Post_ID"].append(post.id)
+                posts_dict1["User_Hash"].append(hash(post.author))#hashes the username to stay within ethical guidelines
+                posts_dict1["orientation"].append(orientation)
         #takes specific fields from the posts, builds list
 
 
-# Saving the data in a pandas dataframe and exporting to csv
 
-subreddit_list = ["communism","socialism", "republican", "libertarian","labouruk","tories","greenandpleasant","conservative","anarcho_capitalism","trump"]
-for sub in subreddit_list:
-        scraper(sub)
-        #loops through list of subreddits i've chosen, and runs the scraper.
+
+rwsubreddit_list = ["republican", "libertarian","tories","conservative","anarcho_capitalism","trump"]
+lwsubreddit_list = ["communism","socialism","labouruk","greenandpleasant","democrats"] 
+for sub in rwsubreddit_list:
+        scraper(sub,"1")
+        #loops through list of right wing subreddits i've chosen, and runs the scraper.
+        #the parameter '1' designates that these are right wing 
+
+for sub in lwsubreddit_list:
+        scraper(sub, "0")
+        #does the same but with the list of left wing subreddits, and 0 to designate that
 posts_file = pd.DataFrame(posts_dict1)
-posts_file.to_csv("posts.csv", mode="a", index=True, header=False)
-#appends the 
+posts_file.to_csv("posts.csv", mode="a", index=True, header=True)
+#appends the data retrieved into a csv file
 
